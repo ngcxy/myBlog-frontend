0a94e0ccdf14bb97fa6f1b281302f9d7d05708ca
ngcxy <caixinyi2013@hotmail.com>
2023-07-17 21:25:49 -0700

    cors issue

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ const baseURL = 'https://my-blog-backend-delta.vercel.app/';
 // const baseURL = 'http://127.0.0.1:8000/';
 
 const axiosInstance = axios.create({
-	// baseURL: baseURL,
+	baseURL: baseURL,
 	timeout: 5000,
 	headers: {
 		Authorization:
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,32 +1,32 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = app => {
-    app.use(
-        createProxyMiddleware('/post',
-            {
-                target: "https://my-blog-backend-delta.vercel.app",
-                changeOrigin: false
-            })
-    )
-    app.use(
-        createProxyMiddleware('/api/token',
-            {
-                target: "https://my-blog-backend-delta.vercel.app",
-                changeOrigin: false
-            })
-    )
-    app.use(
-        createProxyMiddleware('/api/token/blacklist',
-            {
-                target: "https://my-blog-backend-delta.vercel.app",
-                changeOrigin: false
-            })
-    )
-    app.use(
-        createProxyMiddleware('/users/create',
-            {
-                target: "https://my-blog-backend-delta.vercel.app",
-                changeOrigin: false
-            })
-    )
-}
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+//
+// module.exports = app => {
+//     app.use(
+//         createProxyMiddleware('/post',
+//             {
+//                 target: "https://my-blog-backend-delta.vercel.app",
+//                 changeOrigin: false
+//             })
+//     )
+//     app.use(
+//         createProxyMiddleware('/api/token',
+//             {
+//                 target: "https://my-blog-backend-delta.vercel.app",
+//                 changeOrigin: false
+//             })
+//     )
+//     app.use(
+//         createProxyMiddleware('/api/token/blacklist',
+//             {
+//                 target: "https://my-blog-backend-delta.vercel.app",
+//                 changeOrigin: false
+//             })
+//     )
+//     app.use(
+//         createProxyMiddleware('/users/create',
+//             {
+//                 target: "https://my-blog-backend-delta.vercel.app",
+//                 changeOrigin: false
+//             })
+//     )
+// }
